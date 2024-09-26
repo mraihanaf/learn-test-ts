adf0a0996afa67b18b496dbfb83c7124102e7e02
mraihanaf <mraihanaf0@gmail.com>
2024-09-26 22:59:45 +0700

    chore: update husky pre-commit scripts

--- src/__test__/first.test.ts
+++ src/__test__/first.test.ts
@@ -1,9 +1,9 @@
 import { sayHello } from "../index"
 
 describe("first test case!", () => {
-	it("should say Hello World!", () => {
-		expect(sayHello()).toBe("Hello World!")
-	})
+    it("should say Hello World!", () => {
+        expect(sayHello()).toBe("Hello World!")
+    })
 
-	it.todo("make some stuff!")
+    it.todo("make some stuff!")
 })
